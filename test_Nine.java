2c846275181fcfffc5c19b05ecfd02ae1fe6bade
ScorpioGG <scorpiotha.0712@gmail.com>
2026-01-18 15:54:25 +0700

    Finish test 8 and 9

---
+++ test_Nine.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class test_Nine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter you binary number: ");
+        int binaryString = scanner.nextInt();
+
+        System.out.println("Result: " + binary_to_decimal(binaryString));
+    }
+
+    public static int binary_to_decimal(int binary) {
+        int decimalValue = 0;
+        int numBase = 1;
+        int last_Digit = 0;
+        System.out.println("Binary: " + binary + "\n----------");
+
+        while (binary > 0) {
+            last_Digit = binary % 2;
+
+            binary = binary / 2;
+            decimalValue += last_Digit * numBase;
+
+            System.out.println(
+                    "Last Digit: " + last_Digit + "\nPower: " + numBase + "\nDecimal: " + decimalValue
+                            + "\n----------");
+
+            numBase = numBase * 10;
+        }
+        return decimalValue;
+    }
+}
